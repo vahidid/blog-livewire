00e6ba8656d93653221709318f1834854742b84a
Vahid Hasani <vahidhasani09@gmail.com>
2020-09-25 12:05:50 +0330

    add loading state for load content

--- resources/views/livewire/content.blade.php
+++ resources/views/livewire/content.blade.php
@@ -1,4 +1,5 @@
 <div class="flex-1 px-16 overflow-y-auto contentComponent">
+
     <div class="head mt-5 flex items-center justify-between border-b border-red-100 py-5">
         <div class="flex items-center">
             <img src="/assets/img/avatar.png" class="w-12" alt="Avatar">
@@ -11,7 +12,8 @@
         <a href="#" class="bg-red-600 text-white py-1 px-5 rounded-full">Share</a>
     </div>
 
-    <div class="content pt-10 px-2">
+    <div class="content pt-10 px-2 relative" wire:loading.remove wire:target="updateContent">
+        <div class="loader" wire:loading></div>
         {{ $post->content }}
     </div>
 
